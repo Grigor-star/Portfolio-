5daca4729f74a95179918f96cb1e3d075d24c766
Grigor-star <74515267+Grigor-star@users.noreply.github.com>
2022-10-07 17:03:22 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const headerInner = document.querySelector(".header")
+const header = document.querySelector(".header")
+const headerInner = document.querySelector(".header_inner")
 
 window.addEventListener("scroll", function(){
     let scrollPos = window.scrollY;
     
     if(scrollPos > 0) {
-        headerInner.classList.add("inner_2")
+        header.classList.add("inner_2")
+        headerInner.classList.add("inner_3")
     }else {
-         headerInner.classList.remove("inner_2")
+         header.classList.remove("inner_2")
+         headerInner.classList.remove("inner_3")
     }
     
 });
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav_menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    header.classList.toggle("active");
+})
